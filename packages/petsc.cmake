ea5c40164f0d1bf931aee6f98c6f48718bbfa6ca
Shad Durussel <shad.durussel@epfl.ch>
2025-04-29 15:41:35 +0200

    fix typo

--- packages/petsc.cmake
+++ packages/petsc.cmake
@@ -39,7 +39,7 @@ package_declare_sources(petsc
   solver/sparse_matrix_petsc.hh
   solver/sparse_solver_petsc.hh
   solver/sparse_solver_petsc.cc
-  io/parsable_petsc.hh
+  io/parser/parsable_petsc.hh
 )
 
 package_declare_extra_files_to_package(PETSc
